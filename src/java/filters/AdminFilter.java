19698a455dc539efa9d8c9f6f3de843d3bd580c6
822408 <822408@SAIT224224IT.ACDM.DS.SAIT.CA>
2020-11-15 16:32:52 -0700

    Added functionality to AdminFilter Redirects non-administrators to /notes URL

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -1,5 +1,6 @@
 package filters;
 
+import dataaccess.UserDB;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -7,13 +8,39 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
 
 public class AdminFilter implements Filter {
     
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-        throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+            throws IOException, ServletException {
         
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        HttpSession session = httpRequest.getSession();
+        String email = (String) session.getAttribute("email");
+        
+        //get user by email address through UserDB
+        UserDB udb = new UserDB();
+        User user = udb.get(email);
+        
+        //if user's role ID is not 1 (admin), redirect to /notes
+        if (user.getRole().getRoleId() != 1) {
+            httpResponse.sendRedirect("notes");
+            return;
+        }
+        
+        if (email == null) {
+            
+            httpResponse.sendRedirect("login");
+            return;
+        }
+        chain.doFilter(request, response);
     }
     
      @Override
@@ -21,7 +48,7 @@ public class AdminFilter implements Filter {
     }
 
     @Override
-    public void init(FilterConfig filterConfig) {        
+    public void init(FilterConfig filterConfig) throws ServletException {        
         
     }
 }
